54465008c2cdae0ef916863ca61a10fa4098c454
vishal <66064021+vishal368@users.noreply.github.com>
2024-11-14 07:20:33 +0000

    number system

---
+++ javascript/ab.js
@@ -0,0 +1,31 @@
+anum=document.getElementById(har);
+
+// positive and nagitive
+if(anum >=0){
+    document.write("This is positive value")
+}
+else{
+     document.write("This is a negtive number")
+}
+// natural number
+if(anum >=0){
+    document.write("This is Natural number")
+}
+else{
+     document.write("This is a negtive number")
+}
+//this is for prime number
+if(anum/2 >=0){
+    document.write("This is not a prime value")
+}
+else{
+     document.write("This is a Prime value")
+}
+
+//odd even
+if(anum >=0){
+    document.write("This is positive value")
+}
+else{
+     document.write("This is a negtive number")
+}
---
+++ javascript/prime_number.js
@@ -0,0 +1,8 @@
+let number=1;
+
+if (number/2==0){
+    console.log("This is not a prime number")
+}
+else{
+    console.log("This is a prime number")
+}
